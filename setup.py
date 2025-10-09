022c5ccde7953fd1d82b6e330486071bda330bee
namansinghal1505 <namansinghalok@gmail.com>
2025-10-09 19:05:40 +0530

    model Trainer

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
 
     name = "mlproject",
     version = '0.0.1',
-    author = 'Krish',
+    author = 'Naman',
     author_email = 'namansinghaldev@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
